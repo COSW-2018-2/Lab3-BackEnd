e67fd4f1b5a9e72879761a0c6952979496b9d6aa
jhordy salinas <jhordy.salinas@mail.escuelaing.edu.co>
2018-01-31 19:01:10 -0500

    part 2

---
+++ src/main/java/com/eci/cosw/springbootsecureapi/service/TodoServiceImpl.java
@@ -0,0 +1,23 @@
+package com.eci.cosw.springbootsecureapi.service;
+
+import com.eci.cosw.springbootsecureapi.model.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TodoServiceImpl implements TodoService {
+    List<Todo> list = new ArrayList<>();
+
+    @Override
+    public List<Todo> getTodoList() {
+        return list;
+    }
+
+    @Override
+    public Todo addTodo(Todo todo) {
+        list.add(todo);
+        return todo;
+    }
+}
